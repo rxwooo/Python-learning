11d737c2f34a4325243ef318f0e284b38a2128e3
rxwooo <rxwooo@163.com>
2022-01-21 14:32:21 +0800

    simple html

--- flask-learning/watchlist/app.py
+++ flask-learning/watchlist/app.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "welcome to my watchlist"
+    return '<h1>hello totoro!</h1>'\
+        '<img src="http://helloflask.com/totoro.gif">'
