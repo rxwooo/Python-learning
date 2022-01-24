0a08e3f73bd3924aac91dd2ed7985674a21e658f
rxwooo <rxwooo@163.com>
2022-01-25 02:31:16 +0800

    watchlist compeleted

---
+++ flask-learning/watchlist/watchlist/__init__.py
@@ -0,0 +1,34 @@
+import os, sys
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dev'
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    from watchlist.models import User
+    user = User.query.get(int(user_id))
+    return user
+
+@app.context_processor
+def inject_user():
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)
+
+from watchlist import views, commands
